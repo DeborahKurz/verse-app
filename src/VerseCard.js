bc47e958aa9739a227424cab1a3608c739fbb1c8
Deborah Kurz <DeborahKurz6@gmail.com>
2024-02-29 16:21:06 -0700

    VerseCard.js fully functioning

--- src/VerseCard.js
+++ src/VerseCard.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+console.log("VerseCard.js: Finish making Links hyperlinks")
+
 function VerseCard({verse}){
-    console.log(verse);
+    return(
+        <div>
+            <h2>{verse.reference}</h2>
+            <h3>{verse.verse}</h3>
+            <h6>Citation: {verse.url}</h6>
+        </div>
+    )
 };
 
 export default VerseCard;
